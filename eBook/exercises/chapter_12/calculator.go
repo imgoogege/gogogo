d76ab9f7ceb0d8acf3b6ac47569737b9813b5b98
gege <googege@icloud.com>
2018-10-16 15:33:21 +0800

    Update calculator.go

--- eBook/exercises/chapter_12/calculator.go
+++ eBook/exercises/chapter_12/calculator.go
@@ -53,3 +53,101 @@ func main() {
 		}
 	}
 }
+
+
+------------
+
+2018-10-16 自己更新
+
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type OutResult struct {
+	symbol string
+	result int
+}
+
+func main() {
+	Machine()
+}
+
+func Machine() {
+	var resultSlice int
+	var v1 int //中间量
+	outResultValue := new(OutResult)
+	readValue, i := outResultValue.Read()
+	fmt.Println("输出值", readValue)
+	switch readValue[0] {
+	case "+":
+		fmt.Println("是+")
+		for _, v := range readValue[1:] {
+			v1 = transfer(v)
+			resultSlice += v1
+		}
+	case "-":
+		resultSlice = transfer(readValue[1:][0])
+		for _, v := range readValue[2:] {
+			v1 = transfer(v)
+			resultSlice = resultSlice - v1
+		}
+	case "*":
+		resultSlice = 1
+		for _, v := range readValue[1:] {
+			v1 = transfer(v)
+			resultSlice *= v1
+		}
+	case "/":
+		resultSlice = transfer(readValue[1:][0])
+		for _, v := range readValue[2:] {
+			v1 = transfer(v)
+			resultSlice = resultSlice / v1
+		}
+	default:
+		fmt.Println("发生错误")
+	}
+	outResultValue.result = resultSlice
+	outResultValue.symbol = strconv.FormatInt(int64(i-1), 10)
+	fmt.Println(outResultValue)
+}
+func (o *OutResult) String() string {
+	result := strconv.FormatInt(int64(o.result), 10)
+	return "显示结果" + result + "计算的次数" + o.symbol
+}
+func (o *OutResult) Read() ([]string, int) {
+	i := 0
+	fmt.Println("请输入正确的type,仅限 + - * /")
+	re := make([]string, 0)
+	reader := bufio.NewReader(os.Stdin) //创建读
+	for {
+		str, err := reader.ReadString('\n') //记录一次读取，每次读取的分隔符使用\n来确定
+		str = str[0 : len(str)-1]           // remove "\r\n"
+		if err != nil {
+			fmt.Println("25:", err)
+		}
+		if str == "q" {
+			fmt.Println("输入停止，正在计算数值，并且返回给标准输出")
+			break
+		}
+		if i >= 1 {
+			if transfer(str) > 999 {
+				fmt.Println("请勿输入的数值大于999，请重新输入")
+				continue
+			}
+		}
+		re = append(re, str)
+		i++
+	}
+	return re, i
+}
+
+func transfer(v string) int {
+	value, _ := strconv.ParseInt(v, 10, 0)
+	return int(value)
+}
